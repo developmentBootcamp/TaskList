76ed7fcb59f3e752a1564b98a91525539961fcc2
Ali Hassan <bc200401060@vu.edu.pk>
2021-10-04 03:51:18 +0500

    adding the deletion and filtering functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const taskInput = document.querySelector('#task');
 const filter = document.querySelector('#filter');
-const cleatBtn = document.querySelector('.clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 
 //load all eventListeners
 loadEventListeners();
@@ -10,7 +10,13 @@ loadEventListeners();
 //load all eventListeners
 function loadEventListeners() {
   // Add task event
-  form.addEventListener('submit', addTask)
+  form.addEventListener('submit', addTask);
+  // Remove Task event
+  taskList.addEventListener('click', removeTask);
+  // Clear all tasks
+  clearBtn.addEventListener('click', clearTasks);
+  // filter tasks
+  filter.addEventListener('keyup', filterTasks);
 };
 
 // addtask
@@ -29,12 +35,55 @@ function addTask(e){
   const link = document.createElement('a');
   link.className = 'delete-item secondary-content';
   // adding icon HTML
-  link.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  link.innerHTML = '<i class="fas fa-minus-circle"></i>';
   // appeding the link to li
   li.appendChild(link);
   // appening li to ul
   taskList.appendChild(li);
 
+  // clear input
+  taskInput.value = '';
 
   e.preventDefault();
 }
+
+//Remove a Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//clear tasks
+function clearTasks(e){
+  // taskList.innerHTML = ''; // one way
+
+  // if(e.target.classList.contains('clear-tasks')){
+  //   if(confirm('Are you sure?')){
+  //     taskList.remove();
+  //   }
+  // }
+
+  //faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    function(task){
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      }
+      else {
+        task.style.display = 'none';
+      }
+    });
+};
